70a45bc63eb5e95c1519e5e0e8cbec403e02e003
Ilyas <mmoh0227@student.monash.edu>
2025-10-12 02:04:59 +1100

    Created index.html

--- vega-lite-vis.js
+++ vega-lite-vis.js
@@ -1,5 +1,5 @@
-var vg_1 = "test.vg.json";
- vegaEmbed("#bar_chart", vg_1).then(function(result) {
+var vg_1 = "choropleth.vg.json";
+ vegaEmbed("#choropleth", vg_1).then(function(result) {
  // Access the Vega view instance
  // (https://vega.github.io/vega/docs/api/view/) as result.view
  }).catch(console.error);
