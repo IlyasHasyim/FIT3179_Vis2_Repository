dfe0ccb06b8586f7c3b4dfcbaca2f5cc09ffceef
Ilyas <mmoh0227@student.monash.edu>
2025-10-18 04:47:08 +1100

    Fixed HTML and CSS

--- vega-lite-vis.js
+++ vega-lite-vis.js
@@ -3,3 +3,9 @@ var vg_1 = "choropleth.vg.json";
  // Access the Vega view instance
  // (https://vega.github.io/vega/docs/api/view/) as result.view
  }).catch(console.error);
+
+ var vg_2 = "piechart.vg.json";
+  vegaEmbed("#piechart", vg_2).then(function(result) {
+  // Access the Vega view instance
+ // (https://vega.github.io/vega/docs/api/view/) as result.view
+ }).catch(console.error);
